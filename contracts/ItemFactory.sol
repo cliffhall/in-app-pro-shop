dad74a90000f3faf7314dd28aeba9cdd52dc3798
Cliff Hall <cliff@futurescale.com>
2018-08-04 15:51:33 -0400

    * Replaced copied Ownable contract with openzepplin library version. * Changed deployer linking in deploy_contracts migration

--- contracts/ItemFactory.sol
+++ contracts/ItemFactory.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.4.24;
 
-import "./Ownable.sol";
-
+import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 
 contract ItemFactory is Ownable {
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,10 @@
-var Ownable = artifacts.require("./Ownable.sol");
+var Ownable = artifacts.require('openzeppelin-solidity/contracts/ownership/Ownable.sol');
 var ItemFactory = artifacts.require("./ItemFactory.sol");
 var ShopFactory = artifacts.require("./ShopFactory.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Ownable);
-  deployer.link(Ownable, ItemFactory);
+  deployer.link(Ownable, ItemFactory, ShopFactory);
   deployer.deploy(ItemFactory);
-  deployer.link(Ownable, ShopFactory);
   deployer.deploy(ShopFactory);
 };
