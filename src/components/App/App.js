343ba487ef6e9778beae06dd4879dbd977474e81
Cliff Hall <cliff@futurescale.com>
2018-10-22 18:05:26 -0400

    * In components/App/App.js,   - imported DrizzleContext   - wrapped application in DrizzleContext.Provider,   - passed drizzle, drizzleState, and initialized from drizzleContext to Navigation component
    * In components/Nav/Navigation.js,
      - imported accountsFetched and selectAccount action creatorst
      - added componentDidUpdate function, which
        - calls the accountsFetched dispatcher method when initialized changes and if drizzleState.accounts has any accounts in it
        - calls the selectAccount dispatcher when accounts changes (as a result of accountsFetched) passing the first account
      - in render function,
        - removed renderConnectButton function
        - in renderAccountsMenu function,
          - show a disabled 'No accounts' option if there are no accounts
        - in NavBar, when not initialized, show a disabled NavItem 'Connecting...'
      - in mapStateToProps,
        - removed initialize and initializing
        - map accounts and selectedAccount from accountState
      - in mapDispatchToProps,
        - removed initWeb3 dispatcher function
        - mapped accountsFetched and selectAccount dispatcher functions
    
    * Removed services/web3.service.js
    
    * Added store/account/account.actions.js, which
      - declares ACCOUNTS_FETCHED and ACCOUNT_SELECTED action types
      - exports accountsFetched and selectAccount action creators
    
    * Added store/account/account.reducer.js, which
      - imports ACCOUNTS_FETCHED and ACCOUNT_SELECTED action types
      - defines INITIAL_STATE of accounts and selectedAccount, both initialized to null
      - handles ACCOUNTS_FETCHED and ACCOUNT_SELECTED actions
    
    * Removed store/web3/web3.actions.js and web3.reducer.js
    
    * In store/index.js,
      - removed commented out userReducer and walletReducer
      - imported accountReducer and combined into rootReducer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { Panel } from "react-bootstrap";
 import { connect } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { DrizzleContext } from "drizzle-react";
 
 import Navigation from '../Nav/Navigation'
 import { Shop } from '../../domain';
@@ -21,21 +22,27 @@ class App extends Component {
         const DESC = 'Great stuff, cheap!';
 
         let shop = new Shop(OWNER, ID, NAME, DESC);
-        return <Router>
-            <Wrapper>
-                <Navigation/>
-                <Panel>
-                    <Panel.Heading>{shop.name}</Panel.Heading>
-                    <Panel.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit massa a sapien faucibus pellentesque. Fusce laoreet massa in ultricies luctus. Integer quis vulputate est. Quisque luctus eros id neque accumsan ullamcorper. Suspendisse sit amet elit enim. Nam mi nibh, faucibus non dapibus non, tincidunt sit amet lacus. Fusce convallis, nisl eget porttitor dapibus, diam magna finibus est, ut lobortis turpis risus a ipsum.
 
-                        Cras et malesuada mauris. Curabitur vel tortor id est posuere vulputate. Duis quis lorem eu erat ullamcorper bibendum. Fusce in diam quis nibh posuere auctor ac ut orci. Aliquam erat volutpat. Morbi et tincidunt est, congue venenatis mi. Mauris dictum nulla vitae ullamcorper venenatis. Nunc laoreet justo a enim molestie vehicula. Fusce malesuada sem vitae efficitur efficitur. Vestibulum ante lacus, accumsan id odio eget, venenatis feugiat quam. Maecenas in risus sit amet magna porta sodales. Fusce ornare a purus dignissim elementum. Sed sed leo et arcu elementum vehicula vel finibus elit. Sed scelerisque pharetra ante, at vulputate turpis dapibus in. Aliquam a erat ac nulla bibendum bibendum vitae sit amet nisi.
+        return <DrizzleContext.Consumer>
+            {drizzleContext => {
+                const {drizzle, drizzleState, initialized} = drizzleContext;
+                return <Router>
+                    <Wrapper>
+                        <Navigation drizzle={drizzle} drizzleState={drizzleState} initialized={initialized}/>
+                        <Panel>
+                            <Panel.Heading>{shop.name}</Panel.Heading>
+                            <Panel.Body>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit massa a sapien faucibus pellentesque. Fusce laoreet massa in ultricies luctus. Integer quis vulputate est. Quisque luctus eros id neque accumsan ullamcorper. Suspendisse sit amet elit enim. Nam mi nibh, faucibus non dapibus non, tincidunt sit amet lacus. Fusce convallis, nisl eget porttitor dapibus, diam magna finibus est, ut lobortis turpis risus a ipsum.
 
-                        Aenean vel purus dignissim, fermentum risus vitae, hendrerit erat. Curabitur volutpat mi a pretium efficitur. Nunc dignissim euismod augue, nec faucibus eros. Suspendisse imperdiet leo sit amet dui mollis, et laoreet lacus viverra. Quisque nulla justo, varius et turpis non, cursus maximus dolor. In condimentum lectus porta turpis placerat tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vel sapien vitae lorem tempus mollis. Vivamus consectetur convallis ex, at interdum urna ornare sed. Cras cursus convallis enim, a mattis leo ornare at.
-                    </Panel.Body>
-                </Panel>
-            </Wrapper>
-        </Router>
+                                Cras et malesuada mauris. Curabitur vel tortor id est posuere vulputate. Duis quis lorem eu erat ullamcorper bibendum. Fusce in diam quis nibh posuere auctor ac ut orci. Aliquam erat volutpat. Morbi et tincidunt est, congue venenatis mi. Mauris dictum nulla vitae ullamcorper venenatis. Nunc laoreet justo a enim molestie vehicula. Fusce malesuada sem vitae efficitur efficitur. Vestibulum ante lacus, accumsan id odio eget, venenatis feugiat quam. Maecenas in risus sit amet magna porta sodales. Fusce ornare a purus dignissim elementum. Sed sed leo et arcu elementum vehicula vel finibus elit. Sed scelerisque pharetra ante, at vulputate turpis dapibus in. Aliquam a erat ac nulla bibendum bibendum vitae sit amet nisi.
+
+                                Aenean vel purus dignissim, fermentum risus vitae, hendrerit erat. Curabitur volutpat mi a pretium efficitur. Nunc dignissim euismod augue, nec faucibus eros. Suspendisse imperdiet leo sit amet dui mollis, et laoreet lacus viverra. Quisque nulla justo, varius et turpis non, cursus maximus dolor. In condimentum lectus porta turpis placerat tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vel sapien vitae lorem tempus mollis. Vivamus consectetur convallis ex, at interdum urna ornare sed. Cras cursus convallis enim, a mattis leo ornare at.
+                            </Panel.Body>
+                        </Panel>
+                    </Wrapper>
+                </Router>;
+            }}
+        </DrizzleContext.Consumer>;
     }
 }
 
