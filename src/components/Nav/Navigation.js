1c0e71ba3a2b4f18b717205e3bb4f127bf2c22ec
Cliff Hall <cliff@futurescale.com>
2018-11-12 16:11:25 -0500

    * In Navigation.js,   - removed withRouter and wrapper for the connected Navigation component on export

--- src/components/Nav/Navigation.js
+++ src/components/Nav/Navigation.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Navbar, Nav,NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 import { accountsFetched, selectAccount } from '../../store/account/AccountActions';
 import { getShops, selectShop } from '../../store/shop/ShopActions';
 import { getSKUTypes } from '../../store/sku_type/SKUTypeActions';
@@ -144,4 +143,4 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 // Export props-mapped HOC
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Navigation));
+export default connect(mapStateToProps, mapDispatchToProps)(Navigation);
