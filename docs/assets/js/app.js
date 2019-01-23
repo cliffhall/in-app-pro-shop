b116aaabdd9912e70da03e974845820410aa6f24
Cliff Hall <cliff@futurescale.com>
2019-01-23 09:41:13 -0500

    * In /docs/ added supporting components for Flexslider

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,7 +1,7 @@
 // Initialise FlexSlider for Carousels
 $(window).load(function() {
     $('.flexslider').flexslider({
-        animation: "slide",
+        animation: "fade",
         directionNav: true,
         slideshowSpeed: 5000,
         animationSpeed: 600,
